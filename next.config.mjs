73d5c70894686b300c2579a5a7f6350e726e04d3
expired-pineapple <ruthtirusew944@gmail.com>
2024-09-21 12:24:16 +0300

    next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  pageExtensions: [
-      "page.tsx",
-      "page.ts",
-   
-      "ts"
-  ],
     images: {
         remotePatterns: [
             {
